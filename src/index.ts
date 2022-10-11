ccd500cd59e47f451d0c9671ff712737bbd84c5d
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-10-10 22:46:09 -0300

    feat: config database connections

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
 import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
 import { router as routes } from "./routes/index";
 
 dotenv.config();
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@node-api-rest.qlejsii.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log("📦 Connected to Database");
+    app.listen(port, () => {
+      console.log(`http://localhost:${process.env.PORT}`);
+    });
+  })
+  .catch((err) => console.error(err));
 
 export const app = express();
 const port = process.env.PORT;
@@ -16,7 +29,3 @@ app.use(
 app.use(express.json());
 
 app.use(routes);
-
-app.listen(port, () => {
-  console.log(`http://localhost:${process.env.PORT}`);
-});
